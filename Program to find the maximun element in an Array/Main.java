870bbede276cfd102773b31f23242e265fdf7c03
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:31:14 +0530

    Add files via upload

---
+++ Program to find the maximun element in an Array/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    static int maxElement(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter length of an Array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Given Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("Maximum Element of an Array: " + maxElement(arr));
+        }
+    }
+}
