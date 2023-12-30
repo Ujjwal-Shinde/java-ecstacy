5f0990ec2bcd219ee6abc57a1938e193a73924a2
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:32:00 +0530

    Add files via upload

---
+++ Program to search the given element x in the array and if present then return the (last occurred index) index else return -1/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    static int searchInArray(int[] array, int x) {
+        int ans = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == x) {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter length of an Array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Given Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+
+            System.out.println();
+            System.out.println("Enter Number to be searched in an Array: ");
+            int x = sc.nextInt();
+
+            System.out.println("Index of a given element in an Array is: " + searchInArray(arr, x));
+
+        }
+    }
+}
