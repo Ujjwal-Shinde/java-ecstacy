afd4c3a9343b74836b74a339275fe52af40e5591
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:08:11 +0530

    Add files via upload

---
+++ Program to print Reverse Triangular pattern/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter number of Rows : ");
+            int r = sc.nextInt();
+            for (int i = 1; i <= r; i++) {
+                for (int j = 1; j <= r - i + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
