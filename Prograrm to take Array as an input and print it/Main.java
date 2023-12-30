33fb7cffdc2c25e038434ba3a144df7c3c4ad4bb
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:29:55 +0530

    Add files via upload

---
+++ Prograrm to take Array as an input and print it/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter length of an Array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Given Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
