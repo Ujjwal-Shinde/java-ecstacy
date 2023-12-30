6c0055bfc9067535f1dbbc73fcc96de01263d067
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:39:46 +0530

    Add files via upload

---
+++ Program to find the largest element in an Array/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    static int largestElement(int[] array) {
+        int ans = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > ans) {
+                ans = array[i];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter length of an Array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Given Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+
+            System.out.println();
+
+            System.out.println("Largest element in an Array: " + largestElement(arr));
+
+        }
+    }
+}
