af933a6f71fd2d04ad4453993d84efa6ff57e3c2
ujjwalshindee <106895521+Ujjwal-Shinde@users.noreply.github.com>
2023-12-30 14:30:32 +0530

    Add files via upload

---
+++ Program to calculate the sum of all the elements in the given array/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    static int sumOfArrayElements(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter length of an Array: ");
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            System.out.println("Enter " + n + " elements: ");
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println("Given Array: ");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("Sum of an Array Elements: " + sumOfArrayElements(arr));
+        }
+    }
+}
